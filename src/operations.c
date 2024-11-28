f93bffd5dd1587394ec6993fe9c9da78b7a16f28
Egecan Akıncıoğlu <ea32492@seeu.edu.mk>
2024-11-28 19:35:04 +0000

    Added determinant

--- src/operations.c
+++ src/operations.c
@@ -67,3 +67,48 @@ void print_matrix(int rows, int cols, int matrix[10][10]) {
         printf("\n");
     }
 }
+
+void matrix_multiplication(int rows1, int cols1, int mat1[10][10], int rows2, int cols2, int mat2[10][10], int result[10][10]) {
+    if (cols1 != rows2) {
+        printf("Error: Matrix dimensions do not allow multiplication.\n");
+        return;
+    }
+
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < cols1; k++) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
+
+int determinant(int matrix[10][10], int n) {
+    if (n == 1) {
+        return matrix[0][0];
+    }
+
+    int det = 0;
+    int temp[10][10];
+    int sign = 1;
+
+    for (int f = 0; f < n; f++) {
+        int sub_i = 0;
+        for (int i = 1; i < n; i++) {
+            int sub_j = 0;
+            for (int j = 0; j < n; j++) {
+                if (j == f) {
+                    continue;
+                }
+                temp[sub_i][sub_j] = matrix[i][j];
+                sub_j++;
+            }
+            sub_i++;
+        }
+        det += sign * matrix[0][f] * determinant(temp, n - 1);
+        sign = -sign;
+    }
+
+    return det;
+}
